b4e72aed84d2076d30c147504f7f3eecc9d17d79
freddy-crawford <freddy_crawford@yahoo.com>
2018-01-19 20:37:03 -0700

    added file folders for classes and autoloader

---
+++ sql/classes/autoloader.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: macbookpro
+ * Date: 1/19/18
+ * Time: 8:34 PM
+ */
--- sql/sql/tables.sql
+++ sql/sql/tables.sql
@@ -1,10 +1,10 @@
-ALTER DATABASE CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER DATABASE fcrawford CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS profile;
-DROP TABLE IF EXISTS article;
 DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS user;
 
 
 CREATE TABLE user (
@@ -15,13 +15,13 @@ CREATE TABLE user (
 );
 
 CREATE TABLE profile (
-	profileId VARCHAR(32) NOT NULL,
+	profileId BINARY(16) NOT NULL,
 	firstName VARCHAR(32),
 	lastName VARCHAR(32) NOT NULL,
 	PRIMARY KEY(profileId)
 	);
 CREATE TABLE article (
-	articleProfileId VARCHAR(32) NOT NULL,
+	articleProfileId BINARY(16) NOT NULL,
 	articleText VARCHAR(32) NOT NULL,
 	article DATETIME(6) NOT NULL,
 	INDEX(articleProfileId),
@@ -29,12 +29,13 @@ CREATE TABLE article (
 	PRIMARY KEY(articleProfileId)
 );
 CREATE TABLE comment (
-	commentUserId VARCHAR(32) NOT NULL,
+	commentId BINARY(16) NOT NULL,
+	commentUserId BINARY(16) NOT NULL,
 	commentText VARCHAR(32) NOT NULL,
 	commentDate DATETIME(6),
-	INDEX(commentUserId),
+	INDEX(commentId),
 	FOREIGN KEY(commentUserId) REFERENCES user(userId),
-	PRIMARY KEY (commentUserId)
+	PRIMARY KEY (commentId)
 	);
 
 
