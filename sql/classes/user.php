3e6974490bd2b644334b38b29271026381b5fc4c
freddy-crawford <freddy_crawford@yahoo.com>
2018-01-20 17:27:11 -0700

    added class for user

--- sql/classes/user.php
+++ sql/classes/user.php
@@ -5,3 +5,30 @@
  * Date: 1/19/18
  * Time: 8:33 PM
  */
+class User {
+
+	private $userId;
+	private $email;
+	private $phone;
+
+	function __construct( $userId, $email, $phone ) {
+		$this->userId = $userId;
+		$this->email = $email;
+		$this->phone = $phone;
+	}
+
+	function getName() {
+		return $this->userId;
+
+	}
+
+	function getemail() {
+		return $this->email;
+
+
+		function getphone() {
+			return $this->phone;
+		}
+	}
+
+}
